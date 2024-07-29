470ddddf6a4b4e39446d343ba7d9b2b18b232aaf
AhmedArbaz <ah2797764@gmail.com>
2024-07-29 17:09:19 +0500

    Making a hero section in which we make spotlight, and button both took from acerternity ui library fixed a little bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import HeroSection from "@/Components/HeroSection";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
-    <h1 className="text-2xl text-center">Hello</h1>
+    <>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2] text-white">
+    <HeroSection/>
     </main>
+    </>
   );
 }
